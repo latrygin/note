a17a72e251c1c4650d225f573bfb86e19d8a6e9f
latrygin <114460271+latrygin@users.noreply.github.com>
2023-06-13 14:16:11 +0200

    Create s file for l10n (from lecture)

---
+++ lib/utils/l10n/s.dart
@@ -0,0 +1,57 @@
+import 'dart:convert';
+import 'dart:developer' as developer;
+
+import 'package:flutter/services.dart';
+import 'package:flutter/widgets.dart';
+import 'package:flutter_localizations/flutter_localizations.dart';
+
+class S {
+  static const supportedLocales = [
+    Locale('en'),
+    Locale('ru'),
+  ];
+  static const _delegate = CustomLocalizationsDelegate();
+  static const localizationsDelegates = [
+    GlobalMaterialLocalizations.delegate,
+    GlobalWidgetsLocalizations.delegate,
+    GlobalCupertinoLocalizations.delegate,
+    _delegate,
+  ];
+
+  static S of(BuildContext context) {
+    return Localizations.of<S>(context, S)!;
+  }
+
+  final Locale locale;
+  final Map<String, String> translations;
+
+  S._(this.locale, this.translations);
+
+  String get(String key) {
+    final string = translations[key];
+    if (string == null) {
+      developer.log('123');
+    }
+    return string ?? 'unknown';
+  }
+}
+
+class CustomLocalizationsDelegate extends LocalizationsDelegate<S> {
+  const CustomLocalizationsDelegate();
+
+  @override
+  bool isSupported(Locale locale) => S.supportedLocales
+      .map((e) => e.languageCode)
+      .contains(locale.languageCode);
+
+  @override
+  Future<S> load(Locale locale) async {
+    final content =
+        await rootBundle.loadString('l10n/${locale.languageCode}.json');
+    final map = jsonDecode(content) as Map<String, dynamic>;
+    return S._(locale, map.map((key, value) => MapEntry(key, value as String)));
+  }
+
+  @override
+  bool shouldReload(CustomLocalizationsDelegate old) => false;
+}
