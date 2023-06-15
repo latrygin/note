b6b96dfca992739e6f45e67828bfe68152c56971
latrygin <114460271+latrygin@users.noreply.github.com>
2023-06-15 15:30:07 +0200

    Add new names in l10n

--- lib/utils/l10n/s_name.dart
+++ lib/utils/l10n/s_name.dart
@@ -6,12 +6,9 @@ class SName {
   static const String delete = 'delete';
   static const String whatNeeds = 'what_needs';
   static const String priority = 'priority';
-  static const String unknown = 'unknown';
   static const String nonePriority = 'none_priority';
   static const String lowPriority = 'low_priority';
   static const String highPriority = 'high_priority';
   static const String makeTo = 'make_to';
-  // static const String unknown = 'unknown';
-  // static const String unknown = 'unknown';
-  // static const String unknown = 'unknown';
+  static const String newTask = 'new_task';
 }
