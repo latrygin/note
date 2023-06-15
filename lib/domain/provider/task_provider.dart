a41a719d4a56578c4f5b132f2a7ada98c7e7dc3c
latrygin <114460271+latrygin@users.noreply.github.com>
2023-06-15 15:33:41 +0200

    Create provider for future feature local db

---
+++ lib/domain/provider/task_provider.dart
@@ -0,0 +1,47 @@
+import 'package:note/domain/entity/task.dart';
+import 'package:note/domain/facade/task_facade.dart';
+import 'package:note/screen/notes/cubit/notes_state.dart';
+
+class TaskProvider implements TaskFacade {
+  @override
+  Future<void> add({
+    required String text,
+    required TaskImportant important,
+    DateTime? deadline,
+  }) {
+    // TODO: implement add
+    throw UnimplementedError();
+  }
+
+  @override
+  Future<List<Task>> getAll({
+    FilterTask filter = FilterTask.all,
+  }) {
+    // TODO: implement getAll
+    throw UnimplementedError();
+  }
+
+  @override
+  Future<Task> getAt(int index) {
+    // TODO: implement getAt
+    throw UnimplementedError();
+  }
+
+  @override
+  Future<void> removeAt(int index) {
+    // TODO: implement removeAt
+    throw UnimplementedError();
+  }
+
+  @override
+  Future<void> updateAt({
+    required int index,
+    String? text,
+    TaskImportant? important,
+    DateTime? deadline,
+    bool? done,
+  }) {
+    // TODO: implement updateAt
+    throw UnimplementedError();
+  }
+}
