435e76f7168530a0d9d12c1fb63256bf3c8e11f9
latrygin <114460271+latrygin@users.noreply.github.com>
2023-06-15 15:32:27 +0200

    Create facade for task

--- lib/domain/facade/task_facade.dart
+++ lib/domain/facade/task_facade.dart
@@ -1,10 +1,20 @@
 import 'package:note/domain/entity/task.dart';
+import 'package:note/screen/notes/cubit/notes_state.dart';
 
 abstract class TaskFacade {
-  Future<List<Task>> getAll();
+  Future<List<Task>> getAll({FilterTask filter = FilterTask.all});
   Future<Task> getAt(int index);
-  Future<Task> updateAt(int index, Task task);
-  Future<void> changeStatus(int index, bool isDone);
   Future<void> removeAt(int index);
-  Future<void> add(Task task);
+  Future<void> updateAt({
+    required int index,
+    String? text,
+    TaskImportant? important,
+    DateTime? deadline,
+    bool? done,
+  });
+  Future<void> add({
+    required String text,
+    required TaskImportant important,
+    DateTime? deadline,
+  });
 }
