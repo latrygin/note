4998f4154cfaf5cda5b8343773b941b374e963c7
latrygin <114460271+latrygin@users.noreply.github.com>
2023-06-15 15:29:11 +0200

    Change main and create custom main file

--- lib/main.dart
+++ lib/main.dart
@@ -1,16 +1,19 @@
 import 'dart:async';
+import 'dart:developer' as developer;
 
 import 'package:flutter/material.dart';
 import 'package:note/domain/mock/task_mock.dart';
 
 import 'app.dart';
-import 'utils/logger/logger.dart';
 
 void main() {
   runZonedGuarded(
     () {
       runApp(App(taskFacade: TaskMock()));
     },
-    (error, stackTrace) => logger.e(error),
+    (error, stackTrace) => developer.log(
+      error.toString(),
+      stackTrace: stackTrace,
+    ),
   );
 }
