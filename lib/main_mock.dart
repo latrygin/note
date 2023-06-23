91499c3a30aea1551a81cdbb8f4af54e41d728e2
latrygin <114460271+latrygin@users.noreply.github.com>
2023-06-23 23:25:38 +0200

    Change main files

--- lib/main_mock.dart
+++ lib/main_mock.dart
@@ -1,15 +1,28 @@
 import 'dart:async';
 
 import 'package:flutter/material.dart';
-import 'package:note/domain/provider/task/task_provider_mock.dart';
+import 'package:hive_flutter/hive_flutter.dart';
+import 'package:note/domain/provider/revision/local_revision_provider.dart';
+import 'package:note/domain/provider/revision/revision_provider.dart';
+import 'package:note/domain/provider/task/task_provider.dart';
+import 'package:note/domain/service/task/task_service.dart';
 
 import 'app.dart';
 import 'utils/logger/logger.dart';
 
 void main() {
   runZonedGuarded(
-    () {
-      runApp(App(taskFacade: TaskMock()));
+    () async {
+      WidgetsFlutterBinding.ensureInitialized();
+      await Hive.initFlutter();
+      runApp(
+        App(
+          taskProvider: TaskProvider(),
+          taskService: TaskService(),
+          localRevisionProvider: LocalRevisionProvider(),
+          revisionProvider: RevisionProvider(),
+        ),
+      );
     },
     (error, stackTrace) => logger.e(error),
   );
