44e6834bbcfef02e43cdb1b80454a65d8c7ac45d
latrygin <114460271+latrygin@users.noreply.github.com>
2023-07-02 14:32:06 +0200

    Migrate navigator 1.0 to navigator 2.0

---
+++ lib/core/navigation/transition.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/widgets.dart';
+
+class TaskTransitionDelegate
+    extends DefaultTransitionDelegate<RouteTransitionRecord> {
+  const TaskTransitionDelegate() : super();
+  @override
+  Iterable<RouteTransitionRecord> resolve({
+    required List<RouteTransitionRecord> newPageRouteHistory,
+    required Map<RouteTransitionRecord?, RouteTransitionRecord>
+        locationToExitingPageRoute,
+    required Map<RouteTransitionRecord?, List<RouteTransitionRecord>>
+        pageRouteToPagelessRoutes,
+  }) {
+    final results = super.resolve(
+      newPageRouteHistory: newPageRouteHistory,
+      locationToExitingPageRoute: locationToExitingPageRoute,
+      pageRouteToPagelessRoutes: pageRouteToPagelessRoutes,
+    );
+    return results;
+  }
+}
