3722d7bc0313e59868230c4a9e2f8a6f2d365278
latrygin <114460271+latrygin@users.noreply.github.com>
2023-07-02 22:11:35 +0200

    Create integration test for create task

---
+++ integration_test/test_main.dart
@@ -0,0 +1,30 @@
+import 'dart:async';
+
+import 'package:flutter/material.dart';
+import 'package:note/app.dart';
+import 'package:note/core/navigation/delegate.dart';
+import 'package:note/data/mock_datasource/revision_local_mock.dart';
+import 'package:note/data/mock_datasource/revision_remote_mock.dart';
+import 'package:note/data/mock_datasource/task_local_mock.dart';
+import 'package:note/data/mock_datasource/task_remote_mock.dart';
+import 'package:note/utils/logger/logger.dart';
+
+void main() {
+  runZonedGuarded(
+    () {
+      WidgetsFlutterBinding.ensureInitialized();
+      runApp(
+        App(
+          taskLocalDatasource: TaskLocalMock(),
+          taskRemoteDatasource: TaskRemoteMock(),
+          revisionLocalDatasource: RevisionLocalMock(),
+          revisionRemoteDatasource: RevisionRemoteMock(),
+          taskRouterDelegate: TaskRouterDelegate(),
+        ),
+      );
+    },
+    (error, stackTrace) {
+      logger.e('MAIN:', error, stackTrace);
+    },
+  );
+}
