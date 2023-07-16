987392103b02d3d80c8b8773df9865fbedab0c73
latrygin <114460271+latrygin@users.noreply.github.com>
2023-07-17 01:33:32 +0200

    Update integration test

--- integration_test/test_main.dart
+++ integration_test/test_main.dart
@@ -1,30 +1,39 @@
-import 'dart:async';
 
 import 'package:flutter/material.dart';
+import 'package:get_it/get_it.dart';
 import 'package:note/app.dart';
 import 'package:note/core/navigation/delegate.dart';
+import 'package:note/core/navigation/parser.dart';
+import 'package:note/core/navigation/provider.dart';
+import 'package:note/core/navigation/state.dart';
 import 'package:note/data/mock_datasource/revision_local_mock.dart';
 import 'package:note/data/mock_datasource/revision_remote_mock.dart';
 import 'package:note/data/mock_datasource/task_local_mock.dart';
 import 'package:note/data/mock_datasource/task_remote_mock.dart';
-import 'package:note/utils/logger/logger.dart';
+import 'package:note/domain/repository/revision_local_impl.dart';
+import 'package:note/domain/repository/revision_remote_impl.dart';
+import 'package:note/domain/repository/task_local_impl.dart';
+import 'package:note/domain/repository/task_remote_impl.dart';
 
-void main() {
-  runZonedGuarded(
-    () {
-      WidgetsFlutterBinding.ensureInitialized();
-      runApp(
-        App(
-          taskLocalDatasource: TaskLocalMock(),
-          taskRemoteDatasource: TaskRemoteMock(),
-          revisionLocalDatasource: RevisionLocalMock(),
-          revisionRemoteDatasource: RevisionRemoteMock(),
-          taskRouterDelegate: TaskRouterDelegate(),
-        ),
-      );
-    },
-    (error, stackTrace) {
-      logger.e('MAIN:', error, stackTrace);
-    },
-  );
+void main(){
+  final di = GetIt.instance;
+  /// Инициализация Binding
+  WidgetsFlutterBinding.ensureInitialized();
+
+  /// Инициализация DI
+  /// Router delegate
+  di..registerSingleton<TaskRouterDelegate>(TaskRouterDelegate())
+  ..registerSingleton<RouteInformationParser<NavigationStateDTO>>(TaskRouteInformationParser())
+  ..registerSingleton<PlatformRouteInformationProvider>(DebugRouteInformationProvider())
+  ..registerSingleton<RouteObserver>(RouteObserver())
+
+  /// Repository
+  ..registerLazySingleton<TaskLocalDatasource>(TaskLocalMock.new)
+  ..registerLazySingleton<TaskRemoteDatasource>(TaskRemoteMock.new)
+  ..registerLazySingleton<RevisionLocalDatasource>(RevisionLocalMock.new)
+  ..registerLazySingleton<RevisionRemoteDatasource>(RevisionRemoteMock.new);
+
+  /// Запуск проекта
+  runApp(const App());
+
 }
