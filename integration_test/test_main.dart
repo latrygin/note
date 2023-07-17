2e5ca0aacf314add406a95f03fcf2be5ccc601f1
latrygin <114460271+latrygin@users.noreply.github.com>
2023-07-17 02:22:25 +0200

    Minore update

--- integration_test/test_main.dart
+++ integration_test/test_main.dart
@@ -1,8 +1,8 @@
-
 import 'package:flutter/material.dart';
 import 'package:get_it/get_it.dart';
 import 'package:note/app.dart';
 import 'package:note/core/navigation/delegate.dart';
+import 'package:note/core/navigation/navigation.dart';
 import 'package:note/core/navigation/parser.dart';
 import 'package:note/core/navigation/provider.dart';
 import 'package:note/core/navigation/state.dart';
@@ -15,25 +15,28 @@ import 'package:note/domain/repository/revision_remote_impl.dart';
 import 'package:note/domain/repository/task_local_impl.dart';
 import 'package:note/domain/repository/task_remote_impl.dart';
 
-void main(){
+void main() {
   final di = GetIt.instance;
+
   /// Инициализация Binding
   WidgetsFlutterBinding.ensureInitialized();
 
   /// Инициализация DI
   /// Router delegate
-  di..registerSingleton<TaskRouterDelegate>(TaskRouterDelegate())
-  ..registerSingleton<RouteInformationParser<NavigationStateDTO>>(TaskRouteInformationParser())
-  ..registerSingleton<PlatformRouteInformationProvider>(DebugRouteInformationProvider())
-  ..registerSingleton<RouteObserver>(RouteObserver())
+  di
+    ..registerSingleton<Nav>(TaskRouterDelegate())
+    ..registerSingleton<RouteInformationParser<NavigationStateDTO>>(
+        TaskRouteInformationParser())
+    ..registerSingleton<PlatformRouteInformationProvider>(
+        DebugRouteInformationProvider())
+    ..registerSingleton<RouteObserver>(RouteObserver())
 
-  /// Repository
-  ..registerLazySingleton<TaskLocalDatasource>(TaskLocalMock.new)
-  ..registerLazySingleton<TaskRemoteDatasource>(TaskRemoteMock.new)
-  ..registerLazySingleton<RevisionLocalDatasource>(RevisionLocalMock.new)
-  ..registerLazySingleton<RevisionRemoteDatasource>(RevisionRemoteMock.new);
+    /// Repository
+    ..registerLazySingleton<TaskLocalDatasource>(TaskLocalMock.new)
+    ..registerLazySingleton<TaskRemoteDatasource>(TaskRemoteMock.new)
+    ..registerLazySingleton<RevisionLocalDatasource>(RevisionLocalMock.new)
+    ..registerLazySingleton<RevisionRemoteDatasource>(RevisionRemoteMock.new);
 
   /// Запуск проекта
   runApp(const App());
-
 }
